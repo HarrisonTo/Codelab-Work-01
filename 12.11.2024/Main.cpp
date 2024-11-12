ed444910b5bfa9fb839a29421bb645854ff5707e
HarrisonTo <harrison.todd24@bathspa.ac.uk>
2024-11-12 12:41:51 +0000

    Work From Class

---
+++ 12.11.2024/Main.cpp
@@ -0,0 +1,41 @@
+/*
+    Converts an arabic number between 1 and 13 into roman numeral
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // ordinary variables 
+    string myName = "Harrison";
+    int myAge = 18;
+
+    // arrays
+    // mutiple boxes that can store values of the same type
+    // | ? | ? | ? | ? | ? | ? |
+    //   0   1   2   3   4   5
+
+    string pets[5]; // decrypring array
+        // assigning values 
+    pets[0] = "Orio";
+    pets[1] = "Boris";
+    pets[2] = "Lola";
+    pets[3] = "Rover";
+    pets[4] = "Dougle";
+
+    // alternative array
+    string pets[5] = { "Orio", "Boris", "Lola", "Rover", "Dougle" };
+
+    // accessing elements in an array
+    // cout << "I have a pet called " << pet[0] << endl
+    // cout << "I have a pet called " << pet[1] << endl
+    // cout << "I have a pet called " << pet[2] << endl
+    // cout << "I have a pet called " << pet[3] << endl
+    // cout << "I have a pet called " << pet[4] << endl
+
+   
+
+
+}
